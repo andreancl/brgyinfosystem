bb5fed5d683e4db81cca1bf998dadac741d2390d
Andrea Mae Nicole Rafael <andreanclrafael@gmail.com>
2021-09-25 13:58:46 +0800

    first commit

---
+++ Barangay Information System/LogIn.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogIn
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogIn))
+        Me.imgLogo = New System.Windows.Forms.PictureBox()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogIn = New System.Windows.Forms.Button()
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'imgLogo
+        '
+        Me.imgLogo.Image = CType(resources.GetObject("imgLogo.Image"), System.Drawing.Image)
+        Me.imgLogo.Location = New System.Drawing.Point(572, 64)
+        Me.imgLogo.Name = "imgLogo"
+        Me.imgLogo.Size = New System.Drawing.Size(174, 180)
+        Me.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.imgLogo.TabIndex = 0
+        Me.imgLogo.TabStop = False
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUserName.Location = New System.Drawing.Point(510, 311)
+        Me.txtUserName.Multiline = True
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(298, 39)
+        Me.txtUserName.TabIndex = 1
+        Me.txtUserName.Text = "Username"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(510, 377)
+        Me.txtPassword.Multiline = True
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(298, 39)
+        Me.txtPassword.TabIndex = 2
+        Me.txtPassword.Text = "Password"
+        '
+        'btnLogIn
+        '
+        Me.btnLogIn.Location = New System.Drawing.Point(593, 488)
+        Me.btnLogIn.Name = "btnLogIn"
+        Me.btnLogIn.Size = New System.Drawing.Size(143, 40)
+        Me.btnLogIn.TabIndex = 3
+        Me.btnLogIn.Text = "Log In"
+        Me.btnLogIn.UseVisualStyleBackColor = True
+        '
+        'frmLogIn
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(218, Byte), Integer), CType(CType(232, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1306, 592)
+        Me.Controls.Add(Me.btnLogIn)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.imgLogo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmLogIn"
+        Me.Text = "Log In"
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents imgLogo As System.Windows.Forms.PictureBox
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogIn As System.Windows.Forms.Button
+
+End Class
---
+++ Barangay Information System/LogIn.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmLogIn
+
+    Private Sub LogIn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
