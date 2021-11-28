1da19411bea50f7de6b2ecad444ac43b76a9004f
Andrea Mae Nicole Rafael <91358542+andreancl@users.noreply.github.com>
2021-11-28 21:38:26 +0800

    Update brgyinfodb.sql

--- brgyinfodb.sql
+++ brgyinfodb.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: brgyinfodb
 -- ------------------------------------------------------
--- Server version	5.7.35-log
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `residents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `residents` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int NOT NULL AUTO_INCREMENT,
   `ResidentId` varchar(50) NOT NULL,
   `LastName` varchar(45) DEFAULT NULL,
   `FirstName` varchar(45) DEFAULT NULL,
@@ -35,12 +35,12 @@ CREATE TABLE `residents` (
   `CivilStatus` varchar(45) DEFAULT NULL,
   `Religion` varchar(45) DEFAULT NULL,
   `Nationality` varchar(45) DEFAULT NULL,
-  `ContactNo` bigint(20) DEFAULT NULL,
+  `ContactNo` bigint DEFAULT NULL,
   `Address` varchar(45) DEFAULT NULL,
   `DateTimeCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateTimeUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
   `Remarks` varchar(45) DEFAULT NULL,
-  `Age` int(11) DEFAULT NULL,
+  `Age` int DEFAULT NULL,
   PRIMARY KEY (`ResidentId`),
   UNIQUE KEY `ID_UNIQUE` (`Id`),
   UNIQUE KEY `RESIDENT_ID_UNIQUE` (`ResidentId`)
@@ -65,8 +65,8 @@ DROP TABLE IF EXISTS `service_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `service_details` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `ServiceId` int(11) NOT NULL,
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `ServiceId` int NOT NULL,
   `ComplaineeName` varchar(45) NOT NULL,
   `DefendantName` varchar(45) NOT NULL,
   `Incident` varchar(45) NOT NULL,
@@ -100,12 +100,12 @@ DROP TABLE IF EXISTS `service_types`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `service_types` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   `DateTimeCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateTimeUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `service_types` (
 
 LOCK TABLES `service_types` WRITE;
 /*!40000 ALTER TABLE `service_types` DISABLE KEYS */;
-INSERT INTO `service_types` VALUES (1,'Blotter','2021-10-21 21:44:03','2021-10-21 21:44:03'),(2,'Business Permit','2021-10-21 21:44:03','2021-10-21 21:44:03'),(3,'Barangay Clearance','2021-10-21 21:44:03','2021-10-21 21:44:03'),(4,'Ayuda','2021-10-21 21:44:03','2021-10-21 21:44:03');
+INSERT INTO `service_types` VALUES (1,'Blotter','2021-10-21 21:44:03','2021-10-21 21:44:03'),(2,'Certificate of Residency','2021-10-21 21:44:03','2021-10-21 21:44:03'),(3,'Barangay Clearance','2021-10-21 21:44:03','2021-10-21 21:44:03'),(4,'Ayuda','2021-10-21 21:44:03','2021-10-21 21:44:03'),(5,'Certificate of Indigency','2021-11-28 21:36:09','2021-11-28 21:36:09');
 /*!40000 ALTER TABLE `service_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,10 +126,10 @@ DROP TABLE IF EXISTS `services`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `services` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) NOT NULL,
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `UserId` int NOT NULL,
   `ServiceTypeId` varchar(45) NOT NULL,
-  `ResidentID` int(11) DEFAULT NULL,
+  `ResidentID` int DEFAULT NULL,
   `ChairmanID` varchar(45) NOT NULL,
   `DateTimeCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateTimeUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -159,7 +159,7 @@ DROP TABLE IF EXISTS `user_types`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_types` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) NOT NULL,
   `DateTimeCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateTimeUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -187,7 +187,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int NOT NULL AUTO_INCREMENT,
   `UserId` varchar(45) NOT NULL,
   `LastName` varchar(45) NOT NULL,
   `FirstName` varchar(45) NOT NULL,
@@ -196,17 +196,18 @@ CREATE TABLE `users` (
   `Sex` varchar(45) NOT NULL,
   `Birthdate` date NOT NULL,
   `Address` varchar(45) NOT NULL,
-  `ContactNumber` bigint(20) NOT NULL,
+  `ContactNumber` bigint NOT NULL,
   `Username` varchar(45) NOT NULL,
   `Password` varchar(100) NOT NULL,
   `Position` varchar(45) NOT NULL,
   `DateTimeCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateTimeUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
   `Remarks` varchar(45) DEFAULT NULL,
-  `UserTypeId` int(11) DEFAULT NULL,
+  `UserTypeId` int DEFAULT NULL,
+  `Age` int DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `UserID_UNIQUE` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +216,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (101,'12345','','Chairman',NULL,NULL,'Female','2000-01-06','Caloocan City',9950573917,'admin','0000','Chairman',NULL,NULL,NULL,1),(102,'67890','Secretary','Secretary',NULL,NULL,'Female','2000-01-06','Caloocan City',9950573917,'secretary','12345','Secretary','2021-10-21 21:39:01','2021-10-21 21:39:01',NULL,2);
+INSERT INTO `users` VALUES (101,'USER281120210903','Rafael','Andrea Nicole','Bautista',NULL,'Female','2000-01-06','Caloocan City',9950573917,'admin','0000','Chairman','2021-11-28 21:32:00','2021-11-28 21:32:00',NULL,1,NULL),(103,'USER281120210900','Aileen','Aileen Joy','Abellar','N/A','Female','2000-09-02','Manila',9950573917,'secretary','qwert123','Secretary','2021-11-28 21:23:00','2021-11-28 21:23:00',NULL,2,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -228,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-28 17:15:58
+-- Dump completed on 2021-11-28 21:38:13
